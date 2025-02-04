74f6510b5d8ead0e25718de0de7e8f3133cfde59
GogAndMagog <sergejsmel6@gmail.com>
2025-02-04 23:17:10 +0300

    feat(Test): add tests of Registration and Authentication and Authorization services

---
+++ src/test/java/org/fizz_buzz/service/AuthenticationServiceTest.java
@@ -0,0 +1,106 @@
+package org.fizz_buzz.service;
+
+import org.fizz_buzz.exception.IncorrectPassword;
+import org.fizz_buzz.model.Session;
+import org.fizz_buzz.model.User;
+import org.fizz_buzz.repository.SessionRepository;
+import org.fizz_buzz.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+class AuthenticationServiceTest {
+
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "pass";
+
+    private static final long SESSION_DURATION_TIME = 60_000L; //1 minute in milliseconds
+
+    private static final UUID SESSION_ID = UUID.fromString("7f834ff8-5493-4600-b911-150fc41b10b5");
+
+    private SessionRepository sessionRepository;
+    private UserRepository userRepository;
+
+    private User testUser;
+    private Session testSession;
+
+    private AuthenticationAndAuthorizationService authenticationAndAuthorizationService;
+
+    @BeforeEach
+    void setUp() {
+
+        sessionRepository = mock(SessionRepository.class);
+        userRepository = mock(UserRepository.class);
+
+        authenticationAndAuthorizationService =
+                new AuthenticationAndAuthorizationService(userRepository, sessionRepository, SESSION_DURATION_TIME);
+    }
+
+    @Test
+    void isAuthorized_CorrectData_True() {
+
+        testUser = createTestUser();
+        testSession = createTestSession(testUser);
+
+        when(sessionRepository.findById(SESSION_ID)).thenReturn(Optional.of(testSession));
+
+        assertTrue(authenticationAndAuthorizationService.isAuthorized(SESSION_ID));
+    }
+
+    @Test
+    void isAuthorized_ExpiresAtExpired_False() {
+
+        testUser = createTestUser();
+        testSession = createTestSession(testUser);
+
+        testSession.setExpiresAt(new Date(System.currentTimeMillis()));
+        when(sessionRepository.findById(SESSION_ID)).thenReturn(Optional.of(testSession));
+
+        assertFalse(authenticationAndAuthorizationService.isAuthorized(SESSION_ID));
+    }
+
+    @Test
+    void createSession_CorrectData_NoException() {
+
+        testUser = createTestUser();
+        testSession = createTestSession(testUser);
+
+        when(userRepository.findByLogin(USERNAME)).thenReturn(Optional.of(testUser));
+
+        when(sessionRepository.save(any(Session.class))).thenReturn(testSession);
+
+        assertNotNull(authenticationAndAuthorizationService.createSession(USERNAME, PASSWORD));
+    }
+
+    @Test
+    void createSession_UserNotExist_Exception() {
+        assertThrows(IncorrectPassword.class, () -> authenticationAndAuthorizationService.createSession(USERNAME, PASSWORD));
+    }
+
+    private User createTestUser() {
+
+        var user = new User();
+        user.setId(1L);
+        user.setLogin(USERNAME);
+        user.setPassword(PASSWORD);
+
+        return user;
+    }
+
+    private Session createTestSession(User user) {
+
+        var session = new Session();
+        session.setId(SESSION_ID);
+        session.setUser(user);
+        session.setExpiresAt(new Date(System.currentTimeMillis() + SESSION_DURATION_TIME));
+
+        return session;
+    }
+}
---
+++ src/test/java/org/fizz_buzz/service/RegistrationServiceTest.java
@@ -0,0 +1,88 @@
+package org.fizz_buzz.service;
+
+import org.fizz_buzz.model.Session;
+import org.fizz_buzz.model.User;
+import org.fizz_buzz.repository.SessionRepository;
+import org.fizz_buzz.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceTest {
+
+    private static final String LOGIN = "user";
+    private static final String PASSWORD = "pass";
+
+    private static final long SESSION_DURATION_TIME = 60_000L; //1 minute in milliseconds
+
+    private static final UUID SESSION_ID = UUID.fromString("7f834ff8-5493-4600-b911-150fc41b10b5");
+
+    private UserRepository userRepository;
+    private SessionRepository sessionRepository;
+    private RegistrationService registrationService;
+
+    private User testUser;
+    private Session testSession;
+
+    @BeforeEach
+    void setUp() {
+
+        userRepository = mock(UserRepository.class);
+        sessionRepository  = mock(SessionRepository.class);
+
+        registrationService = new RegistrationService(userRepository, sessionRepository, SESSION_DURATION_TIME);
+
+        testUser = createTestUser();
+        testSession = createTestSession(testUser);
+    }
+
+    @Test
+    void registerUser_CorrectData_NoException(){
+
+        when(sessionRepository.save(any(Session.class))).thenReturn(testSession);
+
+        assertEquals(SESSION_ID, registrationService.registerUser(LOGIN, PASSWORD));
+    }
+
+    @Test
+    void registerUser_UserRepositoryException_Exception(){
+
+        when(userRepository.save(any(User.class))).thenThrow(RuntimeException.class);
+
+        assertThrows(RuntimeException.class, () -> registrationService.registerUser(LOGIN, PASSWORD));
+    }
+
+    @Test
+    void registerUser_SessionRepositoryException_Exception(){
+
+        when(sessionRepository.save(any(Session.class))).thenThrow(RuntimeException.class);
+
+        assertThrows(RuntimeException.class, () -> registrationService.registerUser(LOGIN, PASSWORD));
+    }
+
+    private User createTestUser(){
+
+        User user = new User();
+        user.setId(1L);
+        user.setLogin(LOGIN);
+        user.setPassword(PASSWORD);
+
+        return user;
+    }
+
+    private Session createTestSession(User user){
+
+        var session = new Session();
+        session.setId(SESSION_ID);
+        session.setUser(user);
+        session.setExpiresAt(new Date(System.currentTimeMillis() + SESSION_DURATION_TIME));
+
+        return session;
+    }
+}
