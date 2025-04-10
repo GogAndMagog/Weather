838dfd6cd5b9154bf77a75cd0c7a64cb8fe12584
GogAndMagog <sergejsmel6@gmail.com>
2025-04-10 13:17:19 +0300

    feat(ErrorHandler): add global error handler to show error page

--- src/main/java/org/fizz_buzz/controller/GlobalExceptionHandler.java
+++ src/main/java/org/fizz_buzz/controller/GlobalExceptionHandler.java
@@ -1,44 +1,16 @@
 package org.fizz_buzz.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.fizz_buzz.exception.WrongCredentialsException;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.method.annotation.HandlerMethodValidationException;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(HandlerMethodValidationException.class)
-    public ResponseEntity<List<String>> handleValidationExceptions(Model model, HandlerMethodValidationException ex) {
-        List<String> errors = new ArrayList<String>();
-
-        ModelAndView modelAndView = new ModelAndView();
-        ex.getAllErrors().forEach(error -> {
-            errors.add(error.getDefaultMessage());
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(value = Exception.class, produces = MediaType.TEXT_HTML_VALUE)
+    public String handleAllExceptions(Exception ex) {
+        return "error";
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
 }
