26009e1a626f0a859aa61b47cdf715219b39b1e3
GogAndMagog <sergejsmel6@gmail.com>
2025-04-18 16:00:23 +0300

    refactor(DB migration): delete unused migration scripts

--- src/main/resources/db/changelog/changeset/V1.0/SQL/3-create-sessions-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "Main"."Sessions"
-(
-    "ID"        uuid    NOT NULL UNIQUE PRIMARY KEY ,
-    "UserId"    integer NOT NULL REFERENCES "Main"."Users",
-    "ExpiresAt" timestamp
-);
--- src/main/resources/db/changelog/changeset/V1.0/SQL/5-create-locations-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "Main"."Locations"
-(
-    "ID"        serial  NOT NULL UNIQUE PRIMARY KEY,
-    "Name"      text    NOT NULL,
-    "UserId"    integer NOT NULL REFERENCES "Main"."Users",
-    "Latitude"  decimal NOT NULL,
-    "Longitude" decimal NOT NULL
-);
