82abccc3a91139c40f1d093bbe8a6c30ab519d34
GogAndMagog <sergejsmel6@gmail.com>
2025-04-26 00:39:07 +0300

    feat(Config): move DB username and pass to environment variables

--- src/main/java/org/fizz_buzz/config/DataSourceConfig.java
+++ src/main/java/org/fizz_buzz/config/DataSourceConfig.java
@@ -2,38 +2,39 @@ package org.fizz_buzz.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
+import java.util.Map;
 
 @Configuration
 @PropertySource("classpath:db.properties")
 @Profile("Develop")
 public class DataSourceConfig {
 
-    @Value("${user}")
+    @Value("${POSTGRES_USER}")
     private String username;
-    @Value("${pass}")
+    @Value("${POSTGRES_PASS}")
     private String password;
     @Value("${db.url}")
     private String dbUrl;
-    @Value("${db.schema}")
-    private String schema;
     @Value("${driver}")
     private String driver;
 
     @Bean
     public DataSource getDataSource() {
+
         HikariConfig config = new HikariConfig();
         config.setDriverClassName(driver);
         config.setJdbcUrl(dbUrl);
         config.setUsername(username);
         config.setPassword(password);
-//        config.setSchema(schema);
         return new HikariDataSource(config);
     }
 
